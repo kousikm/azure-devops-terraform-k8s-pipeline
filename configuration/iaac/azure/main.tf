2f98741dbecba110cbaf6639199e37dfd15a4579
kousikm <kousik.majumder@hotmail.com>
2020-06-30 10:14:14 +0530

    k8 resource deleted

--- configuration/iaac/azure/main.tf
+++ configuration/iaac/azure/main.tf
@@ -2,12 +2,3 @@ provider "azurerm" {
   version = "=2.0.0"
   features {}
 }
-
-provider "kubernetes" {
-  host                   = "${azurerm_kubernetes_cluster.k8s.kube_config.0.host}"
-  username               = "${azurerm_kubernetes_cluster.k8s.kube_config.0.username}"
-  password               = "${azurerm_kubernetes_cluster.k8s.kube_config.0.password}"
-  client_certificate     = "${base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate)}"
-  client_key             = "${base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.client_key)}"
-  cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate)}"
-}
